ceec80835c79178692a458b56e10ca41209c4815
DevClate <gitclate@clatent.com>
2024-06-15 05:49:00 -0400

    updated help

--- functions/public/Test-365ACJobTitle.ps1
+++ functions/public/Test-365ACJobTitle.ps1
@@ -1,3 +1,21 @@
+<#
+.SYNOPSIS
+    Tests if users have a job title assigned and exports the results to Excel, HTML, or the console.
+.DESCRIPTION
+    The Test-365ACJobTitle function tests if users have a job title assigned. It takes an array of users as input and checks if each user has a job title. The function returns a collection of results indicating whether each user has a job title or not.
+.PARAMETER Users
+    Specifies the array of users to test. If not provided, all users will be tested.
+.PARAMETER ExcelFilePath
+    Specifies the path to export the results to an Excel file. If this parameter is specified, the function will use the Export-365ACResultToExcel function to export the results.
+.PARAMETER HtmlFilePath
+    Specifies the path to export the results to an HTML file. If this parameter is specified, the function will use the Export-365ACResultToHtml function to export the results.
+.EXAMPLE
+    Test-365ACJobTitle -Users (Get-MgUser -All) -ExcelFilePath "C:\Results.xlsx"
+    Tests all users and exports the results to an Excel file located at "C:\Results.xlsx".
+.EXAMPLE
+    Test-365ACJobTitle -Users $users -HtmlFilePath "C:\Results.html"
+    Tests the specified users and exports the results to an HTML file located at "C:\Results.html".
+#>
 function Test-365ACJobTitle {
     [CmdletBinding()]
     param (
